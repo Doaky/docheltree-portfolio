25173ee2ea7bf590a00efb49d947960ddde33d88
Doaky <danielocheltree@gmail.com>
2026-04-25 15:04:46 -0400

    Updates

---
+++ assets/projects-BhcOWltD.js
@@ -0,0 +1,1 @@
+import{t as e}from"./index-DXTTm3C2.js";import{t}from"./ProjectsList-DAmKqFLj.js";var n=e(),r={"--bg":`#243c2f`,"--text":`#fff4e4`,"--text-dim":`#eac1b8`,"--accent":`#dfb161`,"--accent-hover":`#edc97a`,"--border":`#0f1208`,"--row-hover":`rgba(223, 177, 97, 0.1)`};function i(){return(0,n.jsx)(t,{theme:r})}export{i as default};
